1de8bda265831d59c44c08d685908ea2677393ac
Gibby Free <33621269+gibbyfree@users.noreply.github.com>
2018-09-28 16:28:38 -0400

    Create bijele.cpp

---
+++ kattis/bijele/bijele.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int k, q, r, b, kn, p;
+    std::cin >> k >> q >> r >> b >> kn >> p;
+    std::cout << 1 - k << " ";
+    std::cout << 1 - q << " ";
+    std::cout << 2 - r << " ";
+    std::cout << 2 - b << " ";
+    std::cout << 2 - kn << " ";
+    std::cout << 8 - p << std::endl;
+}
