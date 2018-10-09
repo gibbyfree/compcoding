487b0bfd8fa60595e7fc99519a9e728cc8bfbe0c
Gibby Free <33621269+gibbyfree@users.noreply.github.com>
2018-10-09 13:09:22 -0400

    Create watermelon.java

---
+++ codeforces/watermelon/watermelon.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Watermelon {
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		
+		int weight = in.nextInt();
+		
+		if((weight % 2 == 0) && weight != 2){
+			System.out.print("YES");
+		}
+		else{
+			System.out.print("NO");
+		}
+	}
+}
