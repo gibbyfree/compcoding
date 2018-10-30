8d0c87e77d7260a63b8ededa5f7492e00396395f
Gibby Free <33621269+gibbyfree@users.noreply.github.com>
2018-10-30 05:34:07 -0400

    Create 30-data-types.rb

---
+++ hackerrank/30-data-types/30-data-types.rb
@@ -0,0 +1,20 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+j = gets.chomp.to_i
+dbl = gets.chomp.to_f
+str = gets.chomp
+
+# Print the sum of both integer variables on a new line.
+puts i + j
+
+# Print the sum of the double variables on a new line.
+puts d + dbl
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+puts s + str
+
